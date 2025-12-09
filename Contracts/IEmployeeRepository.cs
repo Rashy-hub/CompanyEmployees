9068af08cae69110bce9406c211c4483c06ad805
Rashy-hub <yagoubi.r@gmail.com>
2025-12-09 23:09:10 +0100

    Fix parameter order naming in IEmployeeRepository

--- Contracts/IEmployeeRepository.cs
+++ Contracts/IEmployeeRepository.cs
@@ -6,7 +6,7 @@ namespace Contracts
     public interface IEmployeeRepository
     {
         IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges);
-        Employee GetEmployee(Guid employeeId, Guid companyId, bool trackChanges);
+        Employee GetEmployee(Guid companyId, Guid employeeId, bool trackChanges);
         void CreateEmployeeForCompany(Guid companyId, Employee employee);
         void DeleteEmployee(Employee employee);
 
