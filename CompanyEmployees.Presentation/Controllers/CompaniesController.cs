9f58e68d5892175d5cb392983a70cca7d899d82d
Rashy-hub <yagoubi.r@gmail.com>
2025-12-11 22:06:09 +0100

    feat: add full async support to controllers

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -15,56 +15,56 @@ namespace CompanyEmployees.Presentation.Controllers
             _manager = manager;
         }
         [HttpGet]
-        public IActionResult GetCompanies()
+        public async Task<IActionResult> GetCompanies()
         {
 
-            var result = _manager.CompanyService.GetAllCompanies(false);
+            var result = await _manager.CompanyService.GetAllCompaniesAsync(false);
             return Ok(result);
 
 
         }
         [HttpGet("{id:guid}", Name = "CompanyById")]
-        public IActionResult GetCompany(Guid id)
+        public async Task<IActionResult> GetCompany(Guid id)
         {
-            var result = _manager.CompanyService.GetCompanyById(id, false);
+            var result = await _manager.CompanyService.GetCompanyByIdAsync(id, false);
             return Ok(result);
         }
 
         [HttpGet("collection/{ids?}", Name = "CompanyCollection")]
 
-        public IActionResult GetCompanies([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        public async Task<IActionResult> GetCompanies([ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
         {
-            var result = _manager.CompanyService.GetCompaniesByIds(ids, false);
+            var result = await _manager.CompanyService.GetCompaniesByIdsAsync(ids, false);
             return Ok(result);
         }
 
         [HttpPost]
-        public IActionResult CreateCompany([FromBody] CompanyForCreationDto company)
+        public async Task<IActionResult> CreateCompany([FromBody] CompanyForCreationDto company)
         {
             if (!ModelState.IsValid)
                 return UnprocessableEntity(ModelState);
-            var companyDto = _manager.CompanyService.CreateCompany(company);
+            var companyDto = await _manager.CompanyService.CreateCompanyAsync(company);
             return CreatedAtRoute("CompanyById", new { id = companyDto.Id }, companyDto);
         }
 
         [HttpPost("collection")]
-        public IActionResult CreateCompanyCollection([FromBody] IEnumerable<CompanyForCreationDto> companies)
+        public async Task<IActionResult> CreateCompanyCollection([FromBody] IEnumerable<CompanyForCreationDto> companies)
         {
 
-            var result = _manager.CompanyService.CreateCompanyCollection(companies);
+            var result = await _manager.CompanyService.CreateCompanyCollectionAsync(companies);
             return CreatedAtRoute("CompanyCollection", new { result.ids }, result.companies);
         }
 
         [HttpDelete("{id:guid}")]
-        public IActionResult DeleteCompany(Guid id) 
+        public async Task<IActionResult> DeleteCompany(Guid id)
         {
-            _manager.CompanyService.DeleteCompany(id, trackChanges: false);
+            await _manager.CompanyService.DeleteCompanyAsync(id, trackChanges: false);
             return NoContent();
         }
         [HttpPut("{id:guid}")]
-        public IActionResult UpdateCompany(Guid id, [FromBody] CompanyForUpdateDto company )
+        public async Task<IActionResult> UpdateCompany(Guid id, [FromBody] CompanyForUpdateDto company)
         {
-            _manager.CompanyService.UpdateCompany(id,company,trackChanges: true);
+            await _manager.CompanyService.UpdateCompanyAsync(id, company, trackChanges: true);
             return NoContent();
         }
 
--- CompanyEmployees.Presentation/Controllers/EmployeeController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeeController.cs
@@ -16,55 +16,55 @@ namespace CompanyEmployees.Presentation.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetEmployees(Guid companyId)
+        public async Task<IActionResult> GetEmployees(Guid companyId)
         {
-            var result = _manager.EmployeeService.GetEmployees(companyId, false);
+            var result = await _manager.EmployeeService.GetEmployeesAsync(companyId, false);
             return Ok(result);
         }
         [HttpGet("{id:guid}", Name = "EmployeeById")]
-        public IActionResult GetEmployeeForCompany(Guid companyId, Guid id)
+        public async Task<IActionResult> GetEmployeeForCompany(Guid companyId, Guid id)
         {
 
-            var result = _manager.EmployeeService.GetEmployee(companyId, id, false);
+            var result = await _manager.EmployeeService.GetEmployeeAsync(companyId, id, false);
             return Ok(result);
         }
         [HttpPost]
-        public IActionResult CreateEmployeeForCompany(Guid companyId, [FromBody] EmployeeForCreationDto employeeForCreationDto)
+        public async Task<IActionResult> CreateEmployeeForCompany(Guid companyId, [FromBody] EmployeeForCreationDto employeeForCreationDto)
         {
             if (!ModelState.IsValid)
                 return UnprocessableEntity(ModelState);
-            var result = _manager.EmployeeService.CreateEmployeeForCompany(companyId, employeeForCreationDto, trackChanges: false);
+            var result = await _manager.EmployeeService.CreateEmployeeForCompanyAsync(companyId, employeeForCreationDto, trackChanges: false);
             return CreatedAtRoute("EmployeeById", new { companyId, id = result.Id }, result);
 
         }
 
         [HttpDelete("{id:guid}")]
-        public IActionResult DeleteEmployeeForCompany(Guid companyId,Guid id) 
+        public async Task<IActionResult> DeleteEmployeeForCompany(Guid companyId, Guid id)
         {
-            _manager.EmployeeService.DeleteEmployeeForCompany(companyId,id, trackChanges:false);
+            await _manager.EmployeeService.DeleteEmployeeForCompanyAsync(companyId, id, trackChanges: false);
             return NoContent();
         }
 
         [HttpPut("{id:guid}")]
-        public IActionResult UpdateEmployeeForCompany(Guid companyId,Guid id, [FromBody] EmployeeForUpdateDto employeeForUpdateDto)
+        public async Task<IActionResult> UpdateEmployeeForCompany(Guid companyId, Guid id, [FromBody] EmployeeForUpdateDto employeeForUpdateDto)
         {
-            _manager.EmployeeService.UpdateEmployeeForCompany(companyId, id, employeeForUpdateDto, comptrackChanges: false, empTrackChanges: true);
+            await _manager.EmployeeService.UpdateEmployeeForCompanyAsync(companyId, id, employeeForUpdateDto, comptrackChanges: false, empTrackChanges: true);
             return NoContent();
         }
 
         [HttpPatch("{id:guid}")]
-        public IActionResult PatchEmployeeForCompany(Guid companyId,Guid id, [FromBody] JsonPatchDocument<EmployeeForUpdateDto> patchDoc)
+        public async Task<IActionResult> PatchEmployeeForCompany(Guid companyId, Guid id, [FromBody] JsonPatchDocument<EmployeeForUpdateDto> patchDoc)
         {
             if (patchDoc is null)
                 return BadRequest("patchDoc object sent from client is null.");
-            var result=_manager.EmployeeService.GetEmployeeForPatch(companyId,id,comptrackChanges:false,empTrackChanges:true);
-            patchDoc.ApplyTo(result.employeeToPatch,ModelState);
+            var result = await _manager.EmployeeService.GetEmployeeForPatchAsync(companyId, id, comptrackChanges: false, empTrackChanges: true);
+            patchDoc.ApplyTo(result.employeeToPatch, ModelState);
             //check also if employeeToPatch is valid for entity
             TryValidateModel(result.employeeToPatch);
             if (!ModelState.IsValid)
                 return UnprocessableEntity(ModelState);
 
-            _manager.EmployeeService.SaveChangesForPatch(result.employeeToPatch, result.employeeEntity);
+            await _manager.EmployeeService.SaveChangesForPatchAsync(result.employeeToPatch, result.employeeEntity);
             return NoContent();
         }
 
