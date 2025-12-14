b2301ee318338784a59ebe422a163ca382a2ef7b
Rashy-hub <yagoubi.r@gmail.com>
2025-12-14 05:36:50 +0100

    Create EmployeForPatchDto with all field optional

---
+++ Shared/DataTransferObjects/EmployeeForPatchDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DataTransferObjects
+{
+    public record EmployeeForPatchDto
+    {
+
+        [MaxLength(20, ErrorMessage = "Maximum length for the Name is 20 characters.")]
+        public string? Name { get; init; }
+
+        [Range(18, 120, ErrorMessage = "Employee age must be between 18 and 120")]
+        public int? Age { get; init; }
+
+        [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
+        public string? Position { get; init; }
+    }
+}
