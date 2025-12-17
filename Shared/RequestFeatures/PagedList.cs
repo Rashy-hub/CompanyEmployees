c9892ac519e93871c66d74125912568cf47665e1
Rashy-hub <yagoubi.r@gmail.com>
2025-12-17 03:06:44 +0100

    Create MetaData and PagedList classes for pagination

---
+++ Shared/RequestFeatures/PagedList.cs
@@ -0,0 +1,21 @@
+﻿namespace Shared.RequestFeatures
+{
+    public class PagedList<T> : List<T>
+    {
+        public MetaData MetaData { get; set; }
+
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+
+            MetaData = new MetaData()
+            {
+                CurrentPage = pageNumber,
+                PageSize = pageSize,
+                TotalCount = count,
+                TotalPage = (int)Math.Ceiling(count / (double)pageSize),
+            };
+            AddRange(items);
+        }
+
+    }
+}
