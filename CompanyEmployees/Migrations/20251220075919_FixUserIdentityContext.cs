5602d0ebeb2ac89d1e0d9d73962e2a93aa1d34fe
Rashy-hub <yagoubi.r@gmail.com>
2025-12-20 19:30:00 +0100

    fix (auth) : add User generic type for IdentityDbContext and apply migration

---
+++ CompanyEmployees/Migrations/20251220075919_FixUserIdentityContext.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CompanyEmployees.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixUserIdentityContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0f70797a-b098-4444-97e3-62fae7672666");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "96585c03-71f9-4ccc-ad02-6cd03826fd6c");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "46095751-d8d4-4a25-82cc-dd952e144268", null, "Administrator", "ADMINISTRATOR" },
+                    { "f96286ba-8794-46b1-9c55-e0c5960980d3", null, "Manager", "MANAGER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "46095751-d8d4-4a25-82cc-dd952e144268");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f96286ba-8794-46b1-9c55-e0c5960980d3");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0f70797a-b098-4444-97e3-62fae7672666", null, "Administrator", "ADMINISTRATOR" },
+                    { "96585c03-71f9-4ccc-ad02-6cd03826fd6c", null, "Manager", "MANAGER" }
+                });
+        }
+    }
+}
